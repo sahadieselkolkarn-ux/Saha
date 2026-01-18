f3b6d378a71faf4f80aa8d108f3c209cd898f3c9
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-01-18 01:57:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5187516946-67996",
+  "appId": "1:556589129076:web:03e9686e0554031c912021",
+  "apiKey": "AIzaSyBMd4vo-pY80I4Nkm9uPvycTM8VE6zAyAs",
+  "authDomain": "studio-5187516946-67996.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "556589129076"
+};
