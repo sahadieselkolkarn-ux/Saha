9fd67561727b4b64671fdb89cdc5605a9e9320bc
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-01-18 03:48:37 +0000

    ทำให้มือถือสแกนลงเวลาได้จริง โดยให้หน้า Kiosk Front (คอมกลางนอกออฟฟิศ) แ

--- src/components/qr-display.tsx
+++ src/components/qr-display.tsx
@@ -1,20 +1,41 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import { cn } from '@/lib/utils';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface QrDisplayProps {
   className?: string;
+  path?: string;
 }
 
-export function QrDisplay({ className }: QrDisplayProps) {
-  // This is a static visual representation of a QR code.
-  // The actual data it would encode is representational.
+export function QrDisplay({ className, path = '/app/scan' }: QrDisplayProps) {
+  const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null);
+
+  useEffect(() => {
+    // This code runs only on the client side, after hydration, to get the full URL.
+    const fullUrl = `${window.location.origin}${path}`;
+    // We use an external API to generate the QR code image from the URL.
+    const qrApiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=256x256&data=${encodeURIComponent(
+      fullUrl
+    )}`;
+    setQrCodeUrl(qrApiUrl);
+  }, [path]);
+
   return (
     <div className={cn('p-4 bg-white rounded-lg shadow-md inline-block', className)}>
-      <svg width="256" height="256" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg" shapeRendering="crispEdges">
-        <path fill="white" d="M0 0h33v33H0z"/>
-        <path fill="black" d="M4 4h7v7H4z M22 4h7v7h-7z M4 22h7v7H4z"/>
-        <path fill="black" d="M6 6h3v3H6z M24 6h3v3h-3z M6 24h3v3H6z"/>
-        <path fill="black" d="M13 4h1v1h-1z M15 4h1v1h-1z M17 4h1v1h-1z M19 4h1v1h-1z M4 13h1v1H4z M13 13h1v1h-1z M15 13h1v1h-1z M16 13h1v1h-1z M18 13h1v1h-1z M20 13h1v1h-1z M22 13h1v1h-1z M24 13h1v1h-1z M26 13h1v1h-1z M28 13h1v1h-1z M4 15h1v1H4z M13 15h1v1h-1z M20 15h1v1h-1z M28 15h1v1h-1z M4 16h1v1H4z M13 16h1v1h-1z M15 16h1v1h-1z M17 16h1v1h-1z M24 16h1v1h-1z M28 16h1v1h-1z M4 18h1v1H4z M15 18h1v1h-1z M18 18h1v1h-1z M22 18h1v1h-1z M24 18h1v1h-1z M4 20h1v1H4z M13 20h1v1h-1z M15 20h1v1h-1z M16 20h1v1h-1z M18 20h1v1h-1z M22 20h1v1h-1z M26 20h1v1h-1z M28 20h1v1h-1z M13 22h1v1h-1z M15 22h1v1h-1z M17 22h1v1h-1z M19 22h1v1h-1z M13 24h1v1h-1z M18 24h1v1h-1z M13 26h1v1h-1z M15 26h1v1h-1z M17 26h1v1h-1z M19 26h1v1h-1z M13 28h1v1h-1z M15 28h1v1h-1z M17 28h1v1h-1z M19 28h1v1h-1z"/>
-      </svg>
+      {qrCodeUrl ? (
+        <Image
+          src={qrCodeUrl}
+          alt={`QR Code for ${path}`}
+          width={256}
+          height={256}
+          priority
+        />
+      ) : (
+        <Skeleton className="w-[256px] h-[256px]" />
+      )}
     </div>
   );
 }
