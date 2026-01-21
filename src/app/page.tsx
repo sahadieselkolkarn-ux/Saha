be33f45a54e2069ffd4f23f82ae05c4db958836a
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-01-21 05:45:48 +0000

    แก้ src/app/page.tsx ให้ redirect ไป /app/jobs ด้วย redirect() จาก next/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
+import { redirect } from "next/navigation";
 export default function Home() {
-  return (
-    <main style={{ padding: 24, fontFamily: "system-ui" }}>
-      <h1>SahaDiesel Service</h1>
-      <p>Root page OK</p>
-      <a href="/app/jobs" style={{ display: "inline-block", padding: 12, border: "1px solid #ccc" }}>
-        Go to App
-      </a>
-    </main>
-  );
+  redirect("/app/jobs");
 }
