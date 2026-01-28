883cfb916a5eb51f355f494bda64c60a74de3e0f
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-01-28 13:30:02 +0000

    ตรวจสอบและแก้ไข Firebase initialization
    ปัญหา:
    - Firebase ถูก initializ

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,13 +1,23 @@
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
+  // This check prevents Firebase from being initialized on the server.
+  if (typeof window === 'undefined') {
+    return {
+      firebaseApp: null,
+      firestore: null,
+      storage: null,
+      auth: null
+    };
+  }
+
   if (getApps().length) {
     return getSdks(getApp());
   }
