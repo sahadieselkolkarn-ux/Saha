42f443531fffa77fcf19b995f6144fc42b7e0747
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-01-31 00:06:21 +0000

    หน้าที่ user แผนกบริหารเข้ามาคือหน้า dashboard บัจจุบันนี้ ok เช็คหน้าซ้

--- app/app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-
-import { PageHeader } from "@/components/page-header";
-
-export default function DashboardPage() {
-  return (
-    <PageHeader title="Dashboard" description="An overview of your business activities." />
-  );
-}
--- app/management/overview/page.tsx
+++
@@ -1,7 +0,0 @@
-import { PageHeader } from "@/components/page-header";
-
-export default function ManagementOverviewPage() {
-  return (
-    <PageHeader title="Dashboard" description="Dashboard for business overview." />
-  );
-}
