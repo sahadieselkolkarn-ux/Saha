4bb8e8819776ee47705211095206b4780f0fc064
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-02-01 05:33:19 +0000

    แก้ 404 เมนูโปรไฟล์พนักงานทั้งหมดที่ลิงก์ไป root path: 1) /settings 2) /

---
+++ app/settings/holidays/layout.tsx
@@ -0,0 +1,3 @@
+export default function HolidaysLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
---
+++ app/settings/my-payslips/layout.tsx
@@ -0,0 +1,3 @@
+export default function MyPayslipsLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
