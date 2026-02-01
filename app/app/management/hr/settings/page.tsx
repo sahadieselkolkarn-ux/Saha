b8939cd0920d59b1c5d12ec46a91ee499be8893b
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-02-01 05:20:06 +0000

    แก้ 404 เมนู “ตั้งค่า HR” ที่เข้า /app/management/hr/settings แล้ว Page

---
+++ app/app/management/hr/settings/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { PageHeader } from "@/components/page-header";
+import { HRSettingsForm } from "@/components/hr-settings-form";
+
+export default function ManagementHRSettingsPage() {
+  return (
+    <>
+      <PageHeader
+        title="ตั้งค่า HR"
+        description="ตั้งค่าการทำงาน, การลา, และการคำนวณเงินเดือน"
+      />
+      <HRSettingsForm />
+    </>
+  );
+}
--- app/management/hr/settings/page.tsx
+++ app/management/hr/settings/page.tsx
@@ -1,16 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { PageHeader } from "@/components/page-header";
-import { HRSettingsForm } from "@/components/hr-settings-form";
-
-export default function ManagementHRSettingsPage() {
-  return (
-    <>
-      <PageHeader
-        title="ตั้งค่า HR"
-        description="ตั้งค่าการทำงาน, การลา, และการคำนวณเงินเดือน"
-      />
-      <HRSettingsForm />
-    </>
-  );
+export default function Page() {
+  redirect("/app/management/hr/settings");
 }
