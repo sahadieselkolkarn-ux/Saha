cc6b08f406d729fe40d66a270702bddefcaddcd9
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-02-03 14:52:30 +0000

    แก้ error ในหน้า /app/management/accounting/cashbook ที่ขึ้นว่า useColle

--- app/app/management/accounting/cashbook/page.tsx
+++ app/app/management/accounting/cashbook/page.tsx
@@ -10,9 +10,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
 import { useFirebase } from "@/firebase";
-import { useCollection } from "@/firebase/firestore/use-collection";
+import { useCollection, type WithId } from "@/firebase/firestore/use-collection";
 import { useDoc } from "@/firebase/firestore/use-doc";
-import type { WithId } from "@/firebase/firestore/use-collection";
 import { useAuth } from "@/context/auth-context";
 import { useToast } from "@/hooks/use-toast";
 import { cn } from "@/lib/utils";
