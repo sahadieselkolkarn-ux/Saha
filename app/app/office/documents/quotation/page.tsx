0dc7eaa123c41d201f0661a8d6ae0f323ab2f813
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-02-11 15:44:33 +0000

    ย้ายเมนู "Template ใบเสนอราคา" มาต่อจาก "ใบกำกับภาษี"

--- app/app/office/documents/quotation/page.tsx
+++ app/app/office/documents/quotation/page.tsx
@@ -1,26 +1,31 @@
-
 "use client";
 
 import Link from "next/link";
 import { PageHeader } from "@/components/page-header";
 import { Button } from "@/components/ui/button";
-import { PlusCircle } from "lucide-react";
+import { PlusCircle, LayoutTemplate } from "lucide-react";
 import { DocumentList } from "@/components/document-list";
 
 export default function OfficeQuotationPage() {
     return (
         <>
             <PageHeader title="ใบเสนอราคา" description="ค้นหาและจัดการใบเสนอราคาทั้งหมด">
-                <div className="flex items-center gap-2">
-                     <Button asChild>
-                        <Link href="/app/office/documents/quotation/new">
-                            <PlusCircle className="mr-2 h-4 w-4" />
-                            สร้างใบเสนอราคาใหม่
+                <div className="flex flex-wrap items-center gap-2">
+                    <Button asChild variant="outline">
+                        <Link href="/app/office/documents/quotation/templates">
+                            <LayoutTemplate className="mr-2 h-4 w-4" />
+                            จัดการ Template
                         </Link>
                     </Button>
                     <Button asChild variant="outline">
                         <Link href="/app/office/jobs/management/quotation">
                             สร้างจากงานซ่อม
+                        </Link>
+                    </Button>
+                    <Button asChild>
+                        <Link href="/app/office/documents/quotation/new">
+                            <PlusCircle className="mr-2 h-4 w-4" />
+                            สร้างใบเสนอราคาใหม่
                         </Link>
                     </Button>
                 </div>
