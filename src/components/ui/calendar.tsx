c16171e29e75d5a3b1e6480e71e170a027c9576b
Thanaphon Noilomtuan <sahadiesel@gmail.com>
2026-02-24 17:44:35 +0000

    ปฏิทินออกมาไม่สวยเหมือนตัวอย่างที่ให้ดูเลย

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -32,9 +32,9 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "flex",
+        head_row: "flex w-full",
         head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem] flex-1 text-center",
         row: "flex w-full mt-2",
         cell: cn(
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
@@ -44,7 +44,7 @@ function Calendar({
         ),
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 flex-1"
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
